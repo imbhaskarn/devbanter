d5693964cc8dfc50fa0f897a34b301426f5b7e8f
bhaskar sharma <imbhaskaran@gmail.com>
2024-03-13 21:24:23 +0530

    remove editor

--- src/app/article/create/page.tsx
+++ src/app/article/create/page.tsx
@@ -1,12 +1,11 @@
 "use client";
-import Editor from "@/components/article/Editor";
 
 const createArticle = () => {
   return (
     <div className="pb-12 pt-12 sm:pb-16 lg:pb-24">
       <div className="px-5 lg:px-0">
         <div className="prose mx-auto sm:prose-lg first-letter:text-4xl first-letter:font-bold first-letter:tracking-[.15em] prose-a:transition prose-a:duration-300 prose-a:ease-in-out hover:prose-a:text-red-700 prose-img:rounded-xl">
-          <Editor />
+       editor
         </div>
       </div>
     </div>
